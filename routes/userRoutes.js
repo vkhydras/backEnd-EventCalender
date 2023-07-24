fb3e8bb2c9c36dc4cb5af4e24506ea32cad96c28
vkhydras <victorkimaru8@gmail.com>
2023-07-24 12:08:00 +0300

    sdfsdf

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,14 @@ router.post('/register', userController.registerUser);
 // Public route - User login
 router.post('/login', userController.loginUser);
 
+// Protected route - Get current user profile (requires authentication)
+router.get('/profile', authMiddleware, userController.getUserProfile);
 
+// Protected route - Update current user profile (requires authentication)
+router.put('/profile', authMiddleware, userController.updateUserProfile);
+
+// Protected route - Delete current user account (requires authentication)
+router.delete('/profile', authMiddleware, userController.deleteUserAccount);
 
 // Other routes...
 
