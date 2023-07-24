ed07535eeb7593752891785558345f19c50e3c09
vkhydras <victorkimaru8@gmail.com>
2023-07-24 11:47:29 +0300

    atempt1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,19 +4,12 @@ const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 // Public route - Register a new user
-router.post('/users/register', userController.registerUser);
+router.post('/register', userController.registerUser);
 
 // Public route - User login
-router.post('/users/login', userController.loginUser);
+router.post('/login', userController.loginUser);
 
-// Protected route - Get current user profile (requires authentication)
-router.get('/profile', authMiddleware, userController.getUserProfile);
 
-// Protected route - Update current user profile (requires authentication)
-router.put('/profile', authMiddleware, userController.updateUserProfile);
-
-// Protected route - Delete current user account (requires authentication)
-router.delete('/profile', authMiddleware, userController.deleteUserAccount);
 
 // Other routes...
 
